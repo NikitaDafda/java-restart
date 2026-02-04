9afbf815f31b3ae353e2e8b7bbfbf13755cf4ca5
NikitaDafda <dafdanikita0@gmail.com>
2026-02-04 17:15:01 +0530

    basic recursion

---
+++ recursion/decreasing.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class decreasing {
+
+    public static void printDec(int n){
+        if(n==1){
+            System.out.println("1");
+            return;
+
+        }
+        System.out.println(n);
+        printDec(n-1);
+    }
+ public static void main(String[] args) {
+    printDec(10);
+ }   
+}
