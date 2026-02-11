56e78d7231a0f1fd2062524b293cf2b5273a1fb8
NikitaDafda <dafdanikita0@gmail.com>
2026-02-11 19:04:45 +0530

    print binary strings which not have consecutive ones

---
+++ recursion2/BinaryString.java
@@ -0,0 +1,20 @@
+package recursion2;
+
+public class BinaryString {
+    // code for binary strings which not have consecutive ones
+    public static void BinaryStr(int n,int lastPlace,String str){
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        
+        BinaryStr(n-1,0,str+"0");
+    
+        if(lastPlace==0) {
+        BinaryStr(n-1,1,str+"1");
+    }
+    }
+    public static void main(String[] args) {
+        BinaryStr(3,0,"");
+    }
+}
