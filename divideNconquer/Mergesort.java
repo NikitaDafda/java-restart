5aebc226773d4638b1323f66e780dbe3df78bda1
NikitaDafda <dafdanikita0@gmail.com>
2026-02-13 15:23:49 +0530

    merge sort code

---
+++ divideNconquer/Mergesort.java
@@ -0,0 +1,62 @@
+package divideNconquer;
+
+public class Mergesort {
+    public static void mergeSort(int arr[],int si,int ei){
+        // si->starting index 
+        //  ei-> ending index
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergeSort(arr,si,mid);
+        mergeSort(arr,mid+1,ei);
+        merge(arr,si,mid,ei);
+    }
+
+        public static void merge(int arr[],int si,int mid,int ei){
+            int temp[]=new int[ei-si+1];
+            int i=si;
+            int j=mid+1;
+            int k=0;
+
+            while(i<=mid && j<=ei){
+                if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        //leftover element of left side
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        //leftover element on rightside
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+        //copy temp array in original array
+        for(k=0 ,i=si;k<temp.length;k++ ,i++){
+            arr[i]=temp[k];
+            
+        }
+    }
+        //printing array
+        public static void printArr(int arr[]){
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }}
+
+
+        
+    
+    public static void main(String[] args) {
+        int arr[]={3,2,5,4,7,1};
+        mergeSort(arr,0,arr.length-1);
+        printArr(arr);
+    }
+
+}
