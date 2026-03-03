7615a08599e4e6cf4e61800fc971bbe8af76b51e
NikitaDafda <dafdanikita0@gmail.com>
2026-03-03 17:01:15 +0530

    checking is An Arraylist is monotonic if it is either monotone increasing or monotone decreasing

---
+++ ArrayList/Monotonic.java
@@ -0,0 +1,28 @@
+package ArrayList;
+import java.util.*;
+
+public class Monotonic {
+    public static boolean isMonotonic(ArrayList<Integer>list){
+        boolean increasing = true;
+        boolean decreasing = true;
+        
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i) > list.get(i+1)){
+                increasing = false;
+            }
+            if(list.get(i) < list.get(i+1)){
+                decreasing = false;
+            }
+        }
+        return increasing || decreasing;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer>list= new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(2);
+       // list.add(5);
+       // list.add(4);
+        System.out.println(isMonotonic(list));
+    }
+}
